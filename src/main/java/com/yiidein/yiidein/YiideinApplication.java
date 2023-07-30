d410d9d7836bb0c585257a01f02681aa1f292cf5
Souleymane SD Diallo <souleymanetimbidiallo@gmail.com>
2023-07-30 20:58:00 +0200

    Initial commit for backend

---
+++ src/main/java/com/yiidein/yiidein/YiideinApplication.java
@@ -0,0 +1,13 @@
+package com.yiidein.yiidein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiideinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiideinApplication.class, args);
+	}
+
+}
